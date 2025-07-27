83ff8f071593c37bda83ca1901892a70335ffe91
Dibyanshu Moura <dibyanshumouraofficial@gmail.com>
2025-07-27 11:40:21 +0530

    Add README.md

--- WORDS REVERSE IN A STRING/myApproach.cpp
+++ WORDS REVERSE IN A STRING/myApproach.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 string reverseWords(string s) {
+    // MY APPROACH WAS TO INSERT EVERY WORD INTO THE ANS ARRAY ANS THEN USING REVERSE LOOP TO ADD THEM AGAIN IN CORRECT ORDER IN THE STRING
     vector<string> ans;
     string cS = "";
-    
+    // FIRST ADD THE CHRACTERS INTO THE cS UNTIL IT ENOUNTERS SPACE ADD THE CURRENT STRING INTO THE ANS IF THE THE CS IS NOT EMPTY 
     for (char ch : s) {
         if (ch != ' ') {
             cS += ch;
@@ -15,18 +16,18 @@ string reverseWords(string s) {
             }
         }
     }
-
+    // ADD THE LAST REMAINING WORD INTO THE ANS ARRAY
     if (!cS.empty()) {
         ans.push_back(cS);
     }
-
+    // WE SET THE ORIGNAL STRING EMPTY AND THEN REVERSE LOOP IN THE ANS AND ADD THE WORD AND THE SPACES IF IT IS NOT THE FIRST WORD(OR THE LAST CAUSE REVERSE) 
     s = "";
     for (int i = ans.size() - 1; i >= 0; --i) {
         s += ans[i];
         if (i != 0)
             s += ' ';
     }
-
+    // WE RETURN THE STRING WITH THE WORDS REVERSED IN IT
     return s;
 }
 
